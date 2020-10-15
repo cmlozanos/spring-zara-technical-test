5275b50b287b2d88e3163464e5df6696084da172
Carlos Lozano <carlosmlozanos@gmail.com>
2020-10-15 10:59:44 +0200

    initial commit

---
+++ src/main/java/com/sngular/springzaratechnicaltest/SpringZaraTechnicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.sngular.springzaratechnicaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringZaraTechnicalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringZaraTechnicalTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sngular/springzaratechnicaltest/SpringZaraTechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sngular.springzaratechnicaltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringZaraTechnicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
